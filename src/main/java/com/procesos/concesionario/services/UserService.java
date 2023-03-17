478aa3a3f68737e55d9a75f562ae4098bc468203
Nicolas <colombia1@gmail.com>
2023-03-16 20:47:29 -0500

    Creacion de servicios y endpoint de crear usuarios

--- src/main/java/com/procesos/concesionario/services/UserService.java
+++ src/main/java/com/procesos/concesionario/services/UserService.java
@@ -1,20 +1,14 @@
 package com.procesos.concesionario.services;
 
 import com.procesos.concesionario.models.User;
-import com.procesos.concesionario.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import java.util.List;
 
-@Service
-public class UserService {
+public interface UserService {
+    User getUserById(Long id);
+    List<User> allUsers();
+    User createUser(User user);
+    User updateUser(Long id, User user);
 
-    @Autowired
-    private UserRepository userRepository;
-
-    public Optional<User> getUserById(Long id){
-      return userRepository.findById(id);
-    }
 
 }
